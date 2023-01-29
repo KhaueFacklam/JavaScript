8e920fbefe465a39b3bc1d0a0cf03c66f8e7ebb4
Khaue Tavares <Khauefacklam@gmail.com>
2023-01-28 21:39:59 -0300

    -

--- Front-End III/01 - OBJECTS.js
+++ Front-End III/01 - OBJECTS.js
@@ -1,18 +1,45 @@
-// prototype
+// PROTOTYPE ===========================================
 
 function Pessoa(nome, idade, email) {
-    this.nome = nome
-    this.idade = idade
-    this.email = email
+  this.nome = nome;
+  this.idade = idade;
+  this.email = email;
 }
 
-var pessoa1 = new Pessoa("Glederson", 40, "glederson@gmail.com")
+var pessoa1 = new Pessoa("Glederson", 40, "glederson@gmail.com");
 
-pessoa1.sexo = "masculino"
+pessoa1.sexo = "masculino";//adiciona propriedade ao OBJETO
 
-Pessoa.prototype.apresentar = function () {
-    console.log(`Olá, meu nome é ${this.nome} e meu email é ${this.email}.`)
+Pessoa.prototype.apresentar = function () {//adiciona propriedade ao TIPO(CLASSE)
+  console.log(`Olá, meu nome é ${this.nome} e meu email é ${this.email}.`);
+};
+
+console.log(pessoa1);
+console.log(pessoa1.apresentar());
+
+
+// FETCH =================================================
+function Game () {
+    
+    this.getDeck = async function (){//opção 1
+        const response = await fetch('url.blablabla')
+        const data = await response.json()
+        const deck_id = data.deck_id
+    }
+
+    this.getDeck = function (){//opção 2
+        fetch('url.blablabla')
+        .then((response) => response.json())
+        .then((data) => {const deck_id = data.deck_id})
+        .catch((err) => console.log(err))
+    }
+
+} 
+
+// ARROW FUNCTION =================================================
+elementoDOM.addEventListener('click', clicou) //sem arrow
+function clicou(){
+    //cod
 }
 
-console.log(pessoa1)
-console.log(pessoa1.apresentar())
+elementoDOM.addEventListener('click', () => {/*cod*/}) //com arrow
